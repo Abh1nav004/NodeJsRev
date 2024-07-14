f02531167b25f7180aad339c6e606104ddc800d6
abh1nav <abhinavsharma00410@gmail.com>
2024-07-14 21:03:44 +0530

    middleware

---
+++ project_01/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const fs = require('fs');
+const users = require('./MOCK_DATA (1).json')
+const app = express();
+const PORT = 8000;
+
+
+//Middleware - Plugin
+app.use(express.urlencoded({extended: false}));
+
+app.use((req,res,next)=>{
+    console.log('Hello from middleware 1');
+    req.myUserName = "abhinavsharma.dev";
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('Hello from middleware 2', req.myUserName);
+    next();
+});
+
+
+// for html rendering 
+app.get('/users',(req,res)=>{
+    const html = `
+    <ul>
+    ${users.map((user)=>`<li>${user.first_name}</li>`).join("")};
+    </ul>
+    `;
+
+    res.send(html);
+});
+
+// for rest api calls for developers
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+});
+
+app
+.route("/api/users/:id")
+.get((req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=>user.id===id);
+    return res.json(user);
+})
+.patch((req,res)=>{
+        return res.json({ status: "pending"});
+})
+.delete((req,res)=>{
+        return res.json({ status: "pending"});
+});
+
+
+app.post('/api/users',(req,res)=>{
+    const body = req.body;
+    users.push({...body, id: users.length+1});
+    fs.writeFile('./MOCK_DATA(1).json', JSON.stringify(users),(err, data)=>{
+        return res.json({status:"success",id:users.length});
+    });
+})
+
+
+app.listen(PORT,()=>console.log(`Server Started at PORT ${PORT}`));
