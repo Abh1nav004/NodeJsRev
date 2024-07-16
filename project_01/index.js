bdd55b8a29483d6af47b4999bf975b568d016a2e
abh1nav <abhinavsharma00410@gmail.com>
2024-07-16 14:54:17 +0530

    mongo db and express

--- project_01/index.js
+++ project_01/index.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const fs = require('fs');
+const mongoose = require('mongoose');
 const users = require('./MOCK_DATA (1).json')
+
+
 const app = express();
 const PORT = 8000;
 
+//Connection
+mongoose.connect('mongodb://localhost:27017/mydatabase')
+.then(()=>console.log('MongoDB connected!')) 
+.catch((err)=>console.log("Mongo Error",err)); 
+
+//Schema
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    jobTitle:{
+        type:String,  
+    },
+    gender:{
+        type:String,
+    },
+})
+
+const User = mongoose.model('user',userSchema);
 
 //Middleware - Plugin
 app.use(express.urlencoded({extended: false}));
-
+app.use(express.json());
 app.use((req,res,next)=>{
     fs.appendFile('log.txt',`${Date.now()}:${req.ip}: ${req.method}:${req.path}\n`,(err,data)=>{
         next();
@@ -50,15 +81,29 @@ app
 });
 
 
-app.post('/api/users',(req,res)=>{
+app.post('/api/users',async(req,res)=>{
     const body = req.body;
-    if(!body || !body.first_name || !body.last_name || !body.email || !body.job_title ){
-        return res.status(400).json({msg:"All fields are required"});
+    if(
+       !body || 
+       !body.first_name || 
+       !body.last_name || 
+       !body.email || 
+       !body.gender || 
+       !body.job_title 
+    ){
+        return res.status(400).json({msg:"All fields are required...."});
     }
-    users.push({...body, id: users.length+1});
-    fs.writeFile('./MOCK_DATA (1).json', JSON.stringify(users),(err, data)=>{
-        return res.status(201).json({status:"success",id:users.length});
+   const result = await User.create({
+        firstName: body.first_name,
+        lastName: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        jobTitle: body.job_title,
     });
+
+    console.log('result',result);
+
+    return res.status(201).json({msg:"User created"});
 });
 
 
