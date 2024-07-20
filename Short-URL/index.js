2da2f438905ec27a97f4d297fa3bc8cbeb1ab67a
abh1nav <abhinavsharma00410@gmail.com>
2024-07-20 18:13:05 +0530

    Url-Shortner done

--- Short-URL/index.js
+++ Short-URL/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const {connectToMongoDB}= require("./connect")
 const PORT = 8001;
 
+connectToMongoDB("mongodb://localhost:27017").then(()=>console.log('MongoDB connected!!'));
+
+const urlRoute = require("./routes/url");
+const URL = require('./models/url');
+const { connect } = require("mongoose");
 const app = express();
 
+app.use(express.json());
+app.use("/url",urlRoute);
+
+app.get('/:shortId',async(req,res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, { $push: {
+        visitHistory:{
+            timestamp: Date.now(),
+        },
+    },
+}
+);
+res.redirect(entry.redirectURL)
+});
 app.listen(PORT,()=> console.log(`Server Started at PORT: ${PORT}`))
