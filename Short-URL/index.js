1c1130620bc2aaff2755bcc9d347024625e39c83
abh1nav <abhinavsharma00410@gmail.com>
2024-07-22 22:18:16 +0530

    Authorization

--- Short-URL/index.js
+++ Short-URL/index.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const path = require("path");
-const {connectToMongoDB}= require("./connect")
-const urlRoute = require("./routes/url");
-const staticRoute = require("./routes/staticRouter")
+const cookieParser = require('cookie-parser')
+const {connectToMongoDB}= require("./connect");
+const {restrictToLoggedinUserOnly,checkAuth} = require('./middlewares/auth');
 const URL = require('./models/url');
 const PORT = 8001;
 
+
+
+//Connection 
 connectToMongoDB("mongodb://localhost:27017").then(()=>console.log('MongoDB connected!!'));
 
+
+//Routes
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
 
 
 
 const { connect } = require("mongoose");
 const app = express();
 
+
+//Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+
+
 
 app.set("view engine","ejs");
 app.set('views',path.resolve('./views'));
@@ -27,8 +40,12 @@ app.get("/test",async(req,res)=>{
         urls:allUrls,
     });
 });
-app.use("/url",urlRoute);
-app.use('/',staticRoute);
+
+
+app.use("/url",restrictToLoggedinUserOnly,urlRoute);
+app.use('/user',userRoute );
+app.use('/',checkAuth,staticRoute);
+
 
 app.get('/url/:shortId',async(req,res)=>{
     const shortId = req.params.shortId;
@@ -43,4 +60,7 @@ app.get('/url/:shortId',async(req,res)=>{
 );
 res.redirect(entry.redirectURL)
 });
-app.listen(PORT,()=> console.log(`Server Started at PORT: ${PORT}`))
+
+
+//Port
+app.listen(PORT,()=> console.log(`Server Started at PORT: ${PORT}`));
